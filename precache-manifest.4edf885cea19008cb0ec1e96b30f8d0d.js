0a50315469f6722abc227038608fbf4968d25406
Marcin Kaczor <marcin.kaczor96@gmail.com>
2020-03-05 01:33:20 +0100

    Updates

---
+++ precache-manifest.4edf885cea19008cb0ec1e96b30f8d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e75e7ebc60dc47e906044f8538b1c01",
+    "url": "/witty/index.html"
+  },
+  {
+    "revision": "5a2206c25aa46dfa9e06",
+    "url": "/witty/static/css/main.3e6c6198.chunk.css"
+  },
+  {
+    "revision": "66248c6e48d025d7d4f8",
+    "url": "/witty/static/js/2.e5698b3f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/witty/static/js/2.e5698b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2206c25aa46dfa9e06",
+    "url": "/witty/static/js/main.8d21c260.chunk.js"
+  },
+  {
+    "revision": "73dae861993c595c5713",
+    "url": "/witty/static/js/runtime-main.a7e52c34.js"
+  }
+]);
