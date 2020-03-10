090a8ff507392beb3c00ec2012cac73f809c5dad
Marcin Kaczor <marcin.kaczor96@gmail.com>
2020-03-10 03:09:22 +0100

    Add like reactions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,16 @@
 /* global IntersectionObserver */
 import React, { useState, useRef, useCallback } from 'react'
 import useFetcher from '../Fetcher'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
 import * as API from '../../constants/api'
 
 import './App.css'
 
 function App () {
   const [page, setPage] = useState(1)
+  const [favs, setFavs] = useState<string[]>([])
   const { data, loading, error, hasMore } = useFetcher({ url: API.LIST, page })
 
   const observer = useRef<IntersectionObserver>()
@@ -21,6 +25,14 @@ function App () {
     if (node) observer.current.observe(node)
   }, [loading, hasMore])
 
+  const handleClick = (id : string) => () => {
+    if (favs.includes(id)) {
+      setFavs(favs => favs.filter(fav => fav !== id))
+    } else {
+      setFavs(favs => [...favs, id])
+    }
+  }
+
   return (
     <div className='container'>
       <div className='logo'>
@@ -33,6 +45,11 @@ function App () {
           <div key={id} className='meme' ref={i + 1 === length ? lastDataItemRef : null}>
             <h1 className='meme__title'>{author}</h1>
             <img className='meme__img' src={`${API.ROOT}/id/${id}/600/400`} alt={id} />
+            <div className='meme__footer'>
+              <button className='meme__button' onClick={handleClick(id)}>
+                <FontAwesomeIcon icon={favs.includes(id) ? faHeartSolid : faHeartRegular} />
+              </button>
+            </div>
           </div>
         ))}
         {loading && 'Loading ...'}
